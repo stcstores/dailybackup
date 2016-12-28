e443e7fa4ae610fe915c631d27b22b285ee65da8
Luke Shiner <luke@lukeshiner.com>
2016-12-28 11:29:27 +0000

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='dailybackup',
+    version='1.0',
+    description='STC Stores daily backup',
+    author='Luke Shiner',
+    packages=find_packages(),
+    )
